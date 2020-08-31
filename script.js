c29232d3392dc91017ac84852502b6c504921c9f
shajanjp <shajanjp@gmail.com>
2020-08-31 18:48:50 +0530

    fix: moves logic to script file

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 let qwertyCharset = "qwertyuiopasdfghjklzxcvbnm";
-let qwertyMap = qwertyCharset.split('');
+let qwertyMap = qwertyCharset.split("");
 
-function makeSameCase(rc, tc){
+function makeSameCase(rc, tc) {
   if (rc == rc.toUpperCase()) {
     return tc.toUpperCase();
-  }
-  else {
+  } else {
     return tc.toLowerCase();
   }
 }
 
-function textToLa(text, wrap=['', '']){
-  let laaText = '';
-  text.split('').forEach((c) => {
+function textToLa(text, wrap = ["", ""]) {
+  let laaText = "";
+  text.split("").forEach((c) => {
     let lc = c.toLowerCase();
-    if(lc == ' '){
+    if (lc == " ") {
       laaText += `${wrap[0]} ${wrap[1]}`;
-    }
-    else if(lc == '.' || lc == ','){
+    } else if (lc == "." || lc == ",") {
       laaText += `${wrap[0]}lc${wrap[1]}`;
+    } else {
+      laaText += `${wrap[0]}l${"a".repeat(qwertyCharset.indexOf(lc) + 1)}${
+        wrap[1]
+      }`;
     }
-    else {
-      laaText += `${wrap[0]}l${"a".repeat(qwertyCharset.indexOf(lc) + 1)}${wrap[1]}`;
-    }
-  })
+  });
   return laaText;
 }
 
-function laToText(laaText, wrap=['','']){
-  let text = '';
-  laaText.split(' ').forEach(lw => {
-    lw.split('l').slice(1).forEach(lc => {
-      let chIndex = lc.split('a').length - 2;
-      if(chIndex >= 0){
-        text += `${wrap[0]}${qwertyMap[chIndex]}${wrap[1]}`;
-      }
-    })
+function laToText(laaText, wrap = ["", ""]) {
+  let text = "";
+  laaText.split(" ").forEach((lw) => {
+    lw.split("l")
+      .slice(1)
+      .forEach((lc) => {
+        let chIndex = lc.split("a").length - 2;
+        if (chIndex >= 0) {
+          text += `${wrap[0]}${qwertyMap[chIndex]}${wrap[1]}`;
+        }
+      });
     text += `${wrap[0]} ${wrap[1]}`;
-  })
+  });
   return text;
 }
+
+const contentText = document.getElementById("content-text");
+const outputText = document.getElementById("output-text");
+const laaInvert = document.getElementById("laa-invert");
+
+function makeRainbow(text) {
+  return `${text}`;
+}
+
+contentText.onkeyup = (e) => {
+  if (laaInvert.checked) {
+    outputText.innerHTML = makeRainbow(laToText(contentText.value));
+  } else {
+    outputText.innerHTML = makeRainbow(textToLa(contentText.value));
+  }
+};
